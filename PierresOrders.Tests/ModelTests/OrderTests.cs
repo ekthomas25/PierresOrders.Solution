442c5c17b19ef29a4b9656475941ae4766119c48
Liz Thomas <thomas.elizabeth.k@gmail.com>
2021-12-17 09:47:43 -0800

    Create Test 2: Fail

--- PierresOrders.Tests/ModelTests/OrderTests.cs
+++ PierresOrders.Tests/ModelTests/OrderTests.cs
@@ -1,5 +1,6 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using PierresOrders.Models;
+using System.Collections.Generic;
 
 namespace PierresOrders.Tests
 {
@@ -9,8 +10,18 @@ namespace PierresOrders.Tests
     [TestMethod] //Test 1
     public void OrderConstructor_CreateInstanceOfOrder_Order()
     {
-      Order newOrder = new Order();
+      Order newOrder = new Order("Title", "Description", 4, 211217);
       Assert.AreEqual(typeof(Order), newOrder.GetType());
+    }
+    
+    [TestMethod] //Test 2
+    public void GetAll_ReturnsEmptyList_OrderList()
+    {
+      List<Order> newOrder = new List<Order> { };
+
+      List<Order> result = Order.GetAll();
+
+      CollectionAssert.AreEqual(newOrder, result);
     }
   }
 }
