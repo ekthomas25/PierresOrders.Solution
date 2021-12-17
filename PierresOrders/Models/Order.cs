54d658b336e8feb27ce81c4932c1bbe2d1e59a0b
Liz Thomas <thomas.elizabeth.k@gmail.com>
2021-12-17 09:48:52 -0800

    Test 2: Passing - Returns Empty List

--- PierresOrders/Models/Order.cs
+++ PierresOrders/Models/Order.cs
@@ -1,7 +1,25 @@
+using System.Collections.Generic;
+
 namespace PierresOrders.Models
 {
   public class Order
   {
-    
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public int Price { get; set; }
+    public int Date { get; set; }
+    private static List<Order> _orderList = new List<Order> { };
+    public Order(string title, string description, int price, int date)
+    {
+      Title = title;
+      Description = description;
+      Price = price;
+      Date = date;
+    }
+
+    public static List<Order> GetAll()
+    {
+      return _orderList;
+    }
   }
 }
